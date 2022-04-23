fcdf86a0e3c565a729460df271a2363092aa383c
Sébastien <sebastien@overflo.tech>
2022-04-23 11:55:05 +0200

    fix lint

--- docs/src/components/HelloWorld.vue
+++ docs/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+	</div>
 </template>
 
 <script lang="ts">
@@ -9,33 +9,33 @@ import { Options, Vue } from "vue-class-component";
 import { mapGetters } from "vuex";
 
 @Options({
-  computed: mapGetters(["getResponseLot"]),
-  props: {
-    msg: String,
-  },
+	computed: mapGetters(["getResponseLot"]),
+	props: {
+		msg: String,
+	},
 })
 export default class HelloWorld extends Vue {
-  msg!: string;
-  // async getStatusWorkFlow(){
-  //   await contract.methods.set(5).send({ from: accounts[0] });
-  // }
+	msg!: string;
+	// async getStatusWorkFlow(){
+	//   await contract.methods.set(5).send({ from: accounts[0] });
+	// }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
