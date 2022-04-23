31d07006f5f5d6426e1d128b808fe831ac87f582
Sébastien <sebastien@overflo.tech>
2022-04-23 12:18:19 +0200

    add dist

---
+++ docs/dist/precache-manifest.e0493c0de5d2a4081048ff3d78f0fde7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022de2986dd835851fb4",
+    "url": "/css/app.c55b8607.css"
+  },
+  {
+    "revision": "20f2ea577c6891e34a5d",
+    "url": "/css/chunk-vendors.d0740a57.css"
+  },
+  {
+    "revision": "620e6e308c35bfa5e5e1f985fd7e25a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a9a50e661de369f9ae0",
+    "url": "/js/about.f2d9f042.js"
+  },
+  {
+    "revision": "1ecf4b158d4433629b0e",
+    "url": "/js/admin.853eebe4.js"
+  },
+  {
+    "revision": "022de2986dd835851fb4",
+    "url": "/js/app.2095314d.js"
+  },
+  {
+    "revision": "20f2ea577c6891e34a5d",
+    "url": "/js/chunk-vendors.c9d52676.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
